3165c09589679613e68d4257d262c57954d07184
Maryna <textconstruction@protonmail.com>
2020-10-19 13:36:08 +0100

    Add menu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.ComponentModel.Design;
+using System;
 
 namespace TODO
 {
@@ -6,7 +7,39 @@ namespace TODO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool appIsRunning = true;
+            do
+            {
+                Console.WriteLine("1. Add todo");
+                Console.WriteLine("2. Exit");
+
+                ConsoleKeyInfo input = Console.ReadKey(true);
+                bool inputOk = input.Key == ConsoleKey.D1 ||input.Key == ConsoleKey.D2;
+
+                while(!inputOk) 
+                {
+                    input = Console.ReadKey(true);
+                    inputOk = input.Key == ConsoleKey.D1 ||input.Key == ConsoleKey.D2;
+                }
+                
+    
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("You pushed 'Add to do'");
+                        Console.ReadKey(true);
+                        break; 
+
+                    case ConsoleKey.D2:
+                        appIsRunning = false;
+                        break;
+                }
+
+
+            } while (appIsRunning);
+
+
+            
         }
     }
 }
