6d4a22fd2c7c250ba07099e55cc0ccb2ef9774cf
Mikael Wiik <psythrone@gmail.com>
2020-10-19 14:23:19 +0200

    Add menu

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace TODO
 {
@@ -6,7 +7,30 @@ namespace TODO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            CursorVisible = false;
+            bool appliationRunning = true;
+
+            do
+            {
+                WriteLine("1. Add todo");
+                WriteLine("2. Exit");
+
+                ConsoleKeyInfo input = ReadKey(true);
+
+                Clear();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        break;
+                    case ConsoleKey.D2:
+                        appliationRunning = false;
+                        break;
+                }
+
+            } while (appliationRunning);
         }
     }
 }
