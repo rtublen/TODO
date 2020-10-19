348a9a986806fc81dded7acee2d931ca06fc7959
Simontaga <simon.tornlund@gmail.com>
2020-10-19 14:36:17 +0200

    Add menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,51 @@ namespace TODO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            while(true)
+            {
+                MainMenu();
+            }
+
+            
+
+
         }
+
+        public static void MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("1. Add todo");
+            Console.WriteLine("2. Exit");
+
+
+            bool validInput = false;
+
+
+            while(!validInput){
+
+                var userKeyInput = Console.ReadKey(true);
+
+                switch (userKeyInput.Key)
+                {
+                    case ConsoleKey.D1:
+                        validInput = true;
+                        //...
+                        
+                        break;
+                    case ConsoleKey.D2:
+                        validInput = true;
+                        //...
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }
+        }
+
+
+
     }
 }
