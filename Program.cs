6b18fecd8462e6667cc701d517776743f2a23953
Marcus Sarnes <mackan.g12@gmail.com>
2020-10-19 14:23:01 +0200

    Add menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,37 @@ namespace TODO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            CursorVisible = false;
+
+            var isApplicationRunning = true;
+
+            do
+            {
+                WriteLine("1. Add todo");
+                WriteLine("2. Exit");
+
+                var menuSelection = ReadKey(true);
+
+                Clear();
+
+                switch (menuSelection.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        break;
+
+                    
+                    case ConsoleKey.D2:
+
+                        isApplicationRunning = false;
+
+                        break;
+
+                }
+
+            } while (isApplicationRunning);
+
         }
     }
 }
