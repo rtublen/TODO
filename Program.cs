1db79931d891241720f2c520d679f66b6cff98e6
Alexander Larsson <alex.larsson.123@hotmail.com>
2020-10-19 14:23:57 +0200

    Add menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,35 @@ namespace TODO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.CursorVisible = false;
+
+            bool appliationRunning = true;
+
+            do
+            {
+                Console.WriteLine("1. Add todo");
+                Console.WriteLine("2. Exit");
+
+                ConsoleKeyInfo input = Console.ReadKey(true);
+
+                Console.Clear();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+
+                        break;
+
+                    case ConsoleKey.D2:
+
+                        appliationRunning = false;
+
+                        break;
+                }
+
+            } while (appliationRunning);
+
         }
     }
 }
