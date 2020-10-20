3a853039a8e378d1649460e14384c353c2d39c1a
Max Gambalat <max.gambalat@gmail.com>
2020-10-20 21:55:32 +0200

    Add menu

--- Program.cs
+++ Program.cs
@@ -6,7 +6,40 @@ namespace TODO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+                CursorVisible = false;
+
+                bool applicationRunning = true;
+
+            do
+            {
+                WriteLine("1.Add todo");
+                WriteLine("2. Exit");
+
+                ConsoleKeyInfo input = ReadKey(true);
+
+                Clear();
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.D1:
+
+
+                        break;
+                    case ConsoleKey.D2:
+
+
+                        applicationRunning = false;
+                        
+                        break;
+
+                }
+
+            } while (applicationRunning);
+
+                
+
         }
     }
 }
