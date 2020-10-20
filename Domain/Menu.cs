90a431fda8a2935fa4b87d2283f6fe0e16c7e6e6
Theodor Nordkvist <teddin2@outlook.com>
2020-10-20 19:22:46 +0200

    Add menu

---
+++ Domain/Menu.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+
+namespace TODO.Domain
+{
+    class Menu
+    {
+        public string[] Items { get; }
+
+        public Menu(string[] items)
+        {
+            if (items == null || items.Length == 0)
+            {
+                throw new NullReferenceException("\nInput must contain at least one string\n");
+            }
+            Items = items;
+        }
+
+        public string GetByIndex(int index)
+        {
+            if (index >= Items.Length)
+            {
+                WriteLine($"\nYou asked for element nr. {index + 1}");
+                throw new IndexOutOfRangeException($"\nThis array only has {Items.Length} elements");
+            }
+            return Items[index];
+        }
+
+        public void CreateNumberedMenu()
+        {
+            Clear();
+            WriteLine();
+            for (int i = 0; i < Items.Length; i++)
+            {
+                WriteLine($"  {(i + 1)}. {Items[i]}");
+            }
+        }
+
+        public void CreateUnorderedList()
+        {
+            Clear();
+            WriteLine();
+            for (int i = 0; i < Items.Length; i++)
+            {
+                WriteLine($"  {Items[i]}");
+            }
+        }
+
+        public void CreateTable(string[] column2)
+        {
+            Clear();
+            WriteLine();
+            if (column2.Length == 0)
+            {
+                throw new NullReferenceException("\nInput must contain at least one string\n");
+            }
+            else if (column2.Length < Items.Length)
+            {
+                throw new IndexOutOfRangeException("\nThe second must be at least as big as the first\n");
+            }
+            int marginRight = FindLongest();
+            for (int i = 0; i < Items.Length; i++)
+            {
+                WriteLine($"  {Items[i].PadRight(marginRight)} {column2[i]}");
+            }
+        }
+
+        public int FindLongest()
+        {
+            int longest = Items[0].Length;
+            if (Items.Length == 1)
+            {
+                return longest;
+            }
+            for (int i = 1; i < Items.Length; i++)
+            {
+                if (Items[i].Length > longest)
+                {
+                    longest = Items[i].Length;
+                }
+            }
+            return longest;
+
+        }
+
+        public int GetMenuLength()
+        {
+            return Items.Length;
+        }
+
+        public string[] AskForInputs()
+        {
+            string[] inputs;
+            CursorVisible = true;
+            Clear();
+            inputs = new string[GetMenuLength()];
+            int marginLeft;
+            int marginTop = 1;
+            CreateUnorderedList();
+            for (int i = 0; i < inputs.Length; i++)
+            {
+                marginLeft = GetByIndex(i).Length + 2;
+                SetCursorPosition(marginLeft, marginTop++);
+                inputs[i] = ReadLine();
+                if (string.IsNullOrEmpty(inputs[i]))
+                {
+                    throw new ArgumentNullException("\n\nEmpty input not allowed!\n\n");
+                }
+            }
+            return inputs;
+        }
+
+        public static ConsoleKey SeekCertainInput(params ConsoleKey[] keys)
+        {
+            ConsoleKeyInfo choice;
+            bool allowed = false;
+
+            do
+            {
+                choice = ReadKey(true);
+                for (int i = 0; i < keys.Length; i++)
+                {
+                    allowed = allowed || (choice.Key == keys[i]);
+                }
+            } while (!allowed);
+            return choice.Key;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using TODO.Domain;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using static System.Console;
 
 namespace TODO
 {
@@ -6,7 +10,28 @@ namespace TODO
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Menu mainMenu = new Menu(new string[] 
+            {
+                 "Add task", "Exit",
+            });
+            mainMenu.CreateNumberedMenu();
+            ConsoleKey[] validChoices =
+                    {
+                        ConsoleKey.D1, ConsoleKey.NumPad1, ConsoleKey.D2,
+                        ConsoleKey.NumPad2,
+                    };
+            ConsoleKey input = Menu.SeekCertainInput(validChoices);
+            switch (input)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    WriteLine("Menuoption 1. Code will be added later...");
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    WriteLine("Menuoption 2. Code will be added later...");
+                    break;
+            }
         }
     }
 }
