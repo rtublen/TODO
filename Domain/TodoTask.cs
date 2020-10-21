f275081e09e57d64d4b44e656ebd80102c3bb4b1
Christian Boman <ohdaesu@hotmail.se>
2020-10-21 11:18:20 +0200

    Add ability to add task

---
+++ Domain/TodoTask.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TODO.Domain
+{
+    class TodoTask
+    {
+        private string description;
+        private DateTime dueDate;
+        public string Description 
+        { 
+            get { return description; } 
+            private set 
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException("Description cannot be left empty");
+                }
+                else
+                {
+                    description = value;
+                }
+            } 
+        }
+        public DateTime DueDate { get { return dueDate; } private set { dueDate = value; } }
+        public TodoTask(string description, DateTime dueDate)
+        {
+            Description = description;
+            DueDate = dueDate;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using TODO.Domain;
 
 namespace TODO
 {
     class Program
     {
+        static List<TodoTask> taskList = new List<TodoTask>();
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
@@ -12,6 +17,8 @@ namespace TODO
 
             do
             {
+                Console.Clear();
+
                 Console.WriteLine("1. Add task");
 
                 Console.WriteLine("2. Exit");
@@ -23,6 +30,8 @@ namespace TODO
                 switch (input.Key)
                 {
                     case ConsoleKey.D1:
+
+                        AddTask();
 
                         break;
 
@@ -36,6 +45,44 @@ namespace TODO
 
             } while (applicationRunning);
 
+        }
+        static void AddTask()
+        {
+
+            Console.Clear();
+
+            Console.WriteLine("    Task: ");
+            Console.WriteLine("Due date: ");
+
+            Console.CursorVisible = true;
+
+            Console.SetCursorPosition("    Task: ".Length, 0);
+            var inputDescription = Console.ReadLine();
+
+            Console.SetCursorPosition("    Task: ".Length, 1);
+            var inputDueDate = Console.ReadLine();
+            var inputDueDateTime = DateTime.Parse(inputDueDate);
+
+            Console.CursorVisible = false;
+
+            Console.WriteLine("\nIs this correct? [Y]es [N]o");
+
+            var inputYesNo = Console.ReadKey(true);
+
+            if(inputYesNo.Key == ConsoleKey.Y)
+            {
+                taskList.Add(new TodoTask(inputDescription, inputDueDateTime));
+
+                Console.Clear();
+
+                Console.WriteLine("Task successfully added");
+                Thread.Sleep(2000);
+            }
+            else { 
+            
+            }
+
+            
         }
     }
 }
