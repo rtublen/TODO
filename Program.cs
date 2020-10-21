7b6ffeca4e8bea213b7b736281ec2e3f60f9ee6d
Oscar Skarler <oskar.skarler@gmail.com>
2020-10-21 11:44:24 +0200

    Add ability to add task

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace TODO
 {
     class Program
     {
+        static Dictionary<string, string> taskList = new Dictionary<string, string>();
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
-
             bool applicationRunning = true;
-
             do
             {
                 Console.WriteLine("1. Add task");
@@ -24,6 +25,7 @@ namespace TODO
                 {
                     case ConsoleKey.D1:
 
+                        AddTask();
                         break;
 
                     case ConsoleKey.D2:
@@ -36,6 +38,42 @@ namespace TODO
 
             } while (applicationRunning);
 
+        }
+
+        private static void AddTask()
+        {
+            bool correctInfo = false;
+            do
+            {
+                Console.CursorVisible = true;
+                Console.WriteLine("    Task: \n\n" +
+                                  "Due date: ");
+                Console.SetCursorPosition(10, 0);
+                string task = Console.ReadLine();
+                Console.SetCursorPosition(10, 2);
+                string dueDate = Console.ReadLine();
+
+                Console.CursorVisible = false;
+                Console.WriteLine("Is this information correct? (Y)es (N)o");
+
+                ConsoleKeyInfo input = Console.ReadKey(true);
+
+                switch (input.Key)
+                {
+                    case ConsoleKey.N:
+
+                        Console.Clear();
+                        break;
+                    case ConsoleKey.Y:
+
+                        Console.WriteLine("Task added!");
+                        Thread.Sleep(2000);
+                        correctInfo = true;
+                        taskList.Add(task, dueDate);
+                        Console.Clear();
+                        break;
+                }
+            } while (!correctInfo);
         }
     }
 }
