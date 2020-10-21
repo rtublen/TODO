6c7ba6d182bc588387b5e30940009bf8ddc78185
Mustafa <mustafasaidhelal@gmail.com>
2020-10-21 11:14:50 +0200

    Public Class and List Created

---
+++ Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TODO
+{
+    public class Task
+    {
+        public string taskName { get; set; }
+        public string dueDate { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading;
+using static System.Console;
+
 
 namespace TODO
 {
@@ -10,9 +15,11 @@ namespace TODO
 
             bool applicationRunning = true;
 
+            List<Task> allTasks = new List<Task>();
+
             do
             {
-                Console.WriteLine("1. Add task");
+                Console.WriteLine("1. Add Task");
 
                 Console.WriteLine("2. Exit");
 
@@ -23,6 +30,59 @@ namespace TODO
                 switch (input.Key)
                 {
                     case ConsoleKey.D1:
+
+                        bool invalidTaskCredentials = true;
+
+                        do
+                        {
+                            Task tasks = new Task();
+
+                            Write("Task: ");
+                            tasks.taskName = ReadLine();
+
+                            Write("Due Date: ");
+                            tasks.dueDate = ReadLine();
+
+                            Clear();
+
+                            WriteLine($"Task: {tasks.taskName}");
+                            WriteLine($"Due Date: {tasks.dueDate}");
+
+                            WriteLine(" ");
+
+                            WriteLine("Is this correct? (Y)es or (N)o.");
+
+                            ConsoleKeyInfo yesNo = ReadKey(true);
+
+                            if (yesNo.Key == ConsoleKey.Y)
+                            {
+                                allTasks.Add(tasks);
+
+                                Clear();
+
+                                WriteLine("Task registered.");
+
+                                Thread.Sleep(2000);
+
+                                Clear();
+
+                                break;
+                            }
+                            else if (yesNo.Key == ConsoleKey.N)
+                            {
+                                Clear();
+                            }
+                            else
+                            {
+                                Clear();
+
+                                WriteLine("Invalid key pressed.");
+
+                                Thread.Sleep(2000);
+
+                                Clear();
+                            }
+                        } while (invalidTaskCredentials);
 
                         break;
 
