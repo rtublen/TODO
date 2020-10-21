916b8d3c43411dda12a5a1419c4bbc3bc5330de3
Simon Törnlund <simon.tornlund@gmail.com>
2020-10-21 11:18:06 +0200

    Add ability to add task

---
+++ Domain/Task.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TODO.Domain
+{
+    class Task
+    {
+
+        private string taskContent;
+
+        private string dueDate;
+
+        public Task(string taskContent,string dueDate)
+        {
+            TaskContent = this.taskContent;
+
+            
+        }
+
+
+        public string TaskContent
+        {
+            get { return taskContent; }
+            set { taskContent = value; }
+        }
+
+        public string DueDate
+        {
+            get
+            {
+                return dueDate;
+            }
+
+            set 
+            {
+                dueDate = value;
+            }
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using TODO.Domain;
 namespace TODO
 {
     class Program
     {
+
+        public static List<Task> tasksList = new List<Task>();
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
@@ -12,18 +15,19 @@ namespace TODO
 
             do
             {
+                Console.Clear();
                 Console.WriteLine("1. Add task");
 
                 Console.WriteLine("2. Exit");
 
                 ConsoleKeyInfo input = Console.ReadKey(true);
 
-                Console.Clear();
+                
 
                 switch (input.Key)
                 {
                     case ConsoleKey.D1:
-
+                        AddTaskMenu();
                         break;
 
                     case ConsoleKey.D2:
@@ -37,5 +41,64 @@ namespace TODO
             } while (applicationRunning);
 
         }
+
+        public static void AddTaskMenu() 
+        {
+            Console.Clear();
+            Console.WriteLine(@"    Task:");
+            Console.WriteLine(@"Due Date:");
+
+            Console.SetCursorPosition(11,0);
+            string taskInput = Console.ReadLine();
+
+            Console.SetCursorPosition(11, 1);
+            string taskDueDate = Console.ReadLine();
+
+            var temporaryTask = new Task(taskInput,taskDueDate);
+
+            if (YesOrNo()) 
+            {
+                tasksList.Add(temporaryTask);
+            }
+            
+
+
+        }
+
+
+        public static bool YesOrNo(string text = "\nIs this correct? (Y)es (N)o")
+        {
+            Console.WriteLine("\n" + text);
+
+            bool validInput = false;
+
+            bool userChoice = false;
+
+            while (!validInput)
+            {
+                var userInput = Console.ReadKey(true);
+
+                switch (userInput.Key)
+                {
+                    case ConsoleKey.Y:
+                        validInput = true;
+                        userChoice = true;
+                        break;
+                    case ConsoleKey.N:
+                        validInput = true;
+                        userChoice = false;
+                        break;
+
+                    default:
+                        break;
+
+
+                }
+            }
+
+            return userChoice;
+
+        }
+
     }
 }
