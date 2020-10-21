57e9f6909e24c7ffea5e43aa94be11ade66d8e30
Emil Skoog <emil.skoog.es@gmail.com>
2020-10-21 12:06:26 +0200

    Add ability to add task

---
+++ Model/Task.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TODO.Model
+{
+    public class Task
+    {
+        public Task(string yourTask, string dueDate)
+        {
+            YourTask = yourTask;
+            DueDate = dueDate;
+        }
+
+        private string yourTask;
+        public string YourTask 
+        {
+            get
+            {
+                return yourTask;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Parameter youTask cannot be null or empty");
+                }
+
+                yourTask = value;
+            }
+
+
+        }
+
+        private string dueDate;
+        public string DueDate
+        {
+            get
+            {
+                return dueDate;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Parameter youTask cannot be null or empty");
+                }
+
+                dueDate = value;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,28 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using static System.Console;
+using TODO.Model;
 
 namespace TODO
 {
     class Program
     {
+        static List<Task> taskList = new List<Task>();
+
+        static ConsoleKeyInfo input;
+        static bool invalidInput;
+
         static void Main(string[] args)
         {
-            Console.CursorVisible = false;
+            CursorVisible = false;
 
             bool applicationRunning = true;
 
             do
             {
-                Console.WriteLine("1. Add task");
+                WriteLine("1. Add task");
 
-                Console.WriteLine("2. Exit");
+                WriteLine("2. Exit");
 
-                ConsoleKeyInfo input = Console.ReadKey(true);
+                input = ReadKey(true);
 
-                Console.Clear();
+                Clear();
 
                 switch (input.Key)
                 {
                     case ConsoleKey.D1:
+
+                        AddTask();
 
                         break;
 
@@ -36,6 +47,54 @@ namespace TODO
 
             } while (applicationRunning);
 
+        }
+
+        private static void AddTask()
+        {
+            bool taskNotRegistered = true;
+
+            do
+            {
+                CursorVisible = true;
+                WriteLine("Task: ");
+                WriteLine("Due Date: ");
+
+                SetCursorPosition(6, 0);
+                string yourTask = ReadLine();
+
+                SetCursorPosition(10, +1);
+                string dueDate = ReadLine();
+
+                Task task = new Task(yourTask, dueDate);
+
+                CursorVisible = false;
+
+                WriteLine("\nIs this correct info? (Y)es (N)o");
+
+                input = ReadKey(true);
+
+                Clear();
+
+                do
+                {
+                    invalidInput = !(input.Key == ConsoleKey.Y || input.Key == ConsoleKey.N);
+                } while (invalidInput);
+
+                if (input.Key == ConsoleKey.Y)
+                {
+                    taskList.Add(task);
+                    taskNotRegistered = false;
+                    WriteLine("Task registered");
+                    Thread.Sleep(2000);
+                }
+                else if (input.Key == ConsoleKey.N)
+                {
+                    taskNotRegistered = true;
+                }
+
+                Clear();
+
+            } while (taskNotRegistered);
         }
     }
 }
