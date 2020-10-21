95aa4b81071cd820a8626680683ed4c61e215649
Anna Skarler <skarler.anna@gmail.com>
2020-10-21 11:24:59 +0200

    Add ability to add task

---
+++ MyTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TODO
+{
+    public class MyTask
+    {
+        public MyTask(string name, string dueDate)
+        {
+            Name = name;
+            DueDate = dueDate;
+        }
+
+        public string Name { get; set; }
+        public string DueDate { get; set; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace TODO
 {
     class Program
     {
+        public static List<MyTask> MyTaskList = new List<MyTask>();
+
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
@@ -24,6 +28,8 @@ namespace TODO
                 {
                     case ConsoleKey.D1:
 
+                        AddTask();
+
                         break;
 
                     case ConsoleKey.D2:
@@ -35,6 +41,63 @@ namespace TODO
                 }
 
             } while (applicationRunning);
+
+        }
+
+        private static void AddTask()
+        {
+
+            while (true)
+            {
+                Console.Clear();
+
+                Console.CursorVisible = true;
+                Console.WriteLine("Task:  ");
+                Console.WriteLine("Due date:  ");
+             
+
+                Console.SetCursorPosition(36, 0);
+
+                var task = Console.ReadLine();
+
+                Console.SetCursorPosition(36, 1);
+
+                var dueDate = Console.ReadLine();
+
+                Console.CursorVisible = false;
+
+                Console.WriteLine("Is this correct? (Y)es (N)o ");
+
+                var isCorrectInput = Console.ReadKey(true).Key;
+
+                while (isCorrectInput != ConsoleKey.Y && isCorrectInput != ConsoleKey.N)
+                {
+                    isCorrectInput = Console.ReadKey(true).Key;
+                }
+
+                switch (isCorrectInput)
+                {
+                    case ConsoleKey.Y:
+
+                        
+                        MyTask newTask = new MyTask(task, dueDate);
+
+                        
+                        MyTaskList.Add(newTask);
+
+                        Console.WriteLine("Task registered.");
+                        Thread.Sleep(2000);
+                        break;
+
+                    case ConsoleKey.N:
+                        continue;
+
+                    default:
+                        continue;
+                }
+                break;
+            }
+            Console.Clear();
 
         }
     }
