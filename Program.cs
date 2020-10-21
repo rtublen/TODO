bd92ca0e218b43dfe567ab7c20c38fbb8867ef63
Alexander Larsson <alex.larsson.123@hotmail.com>
2020-10-21 11:29:47 +0200

    Add ability to add task

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace TODO
 {
     class Program
     {
+        static List<Task> taskList = new List<Task>();
+
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
@@ -24,6 +27,8 @@ namespace TODO
                 {
                     case ConsoleKey.D1:
 
+                        AddTask();
+
                         break;
 
                     case ConsoleKey.D2:
@@ -31,11 +36,61 @@ namespace TODO
                         applicationRunning = false;
 
                         break;
-
                 }
 
             } while (applicationRunning);
 
+        }
+
+        public static void AddTask()
+        {
+            Console.CursorVisible = true;
+
+            string taskDescription;
+            DateTime dueDate;
+
+            bool addInfo = true;
+            
+            do
+            {
+                Console.WriteLine("Task: ");
+                Console.WriteLine("Due date: ");
+
+                Console.SetCursorPosition(10, 0);
+                taskDescription = Console.ReadLine();
+
+                Console.SetCursorPosition(10, 1);
+                dueDate = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+                Console.WriteLine("Is this correct? (Y)es (N)o");
+
+                Task task = new Task(taskDescription, dueDate);
+
+                bool userDidNotTypeYorN = true;
+
+                do
+                {
+                    ConsoleKeyInfo inputYorN = Console.ReadKey(true);
+
+                    if (inputYorN.Key == ConsoleKey.Y)
+                    {
+                        taskList.Add(task);
+
+                        userDidNotTypeYorN = false;
+
+                        addInfo = false;
+                    }
+                    else if (inputYorN.Key == ConsoleKey.N)
+                    {
+                        userDidNotTypeYorN = false;
+                    }
+
+                } while (userDidNotTypeYorN);
+
+            } while (addInfo);
+
+            Console.Clear();
         }
     }
 }
---
+++ Task.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TODO
+{
+    class Task
+    {
+        public Task(string taskDescription, DateTime dueDate)
+        {
+            TaskDescription = taskDescription;
+            DueDate = dueDate;
+        }
+
+        public string TaskDescription { get; }
+
+        public DateTime DueDate { get; }
+    }
+}
