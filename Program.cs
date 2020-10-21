1fe9c2f04aa50d2618a2280ded6e9ef869066d05
Marcus Sarnes <mackan.g12@gmail.com>
2020-10-21 11:21:45 +0200

    Add ability to add task to list

--- Program.cs
+++ Program.cs
@@ -1,29 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using static System.Console;
 
 namespace TODO
 {
     class Program
     {
+        public static List<Tasks> taskList = new List<Tasks>();
+
         static void Main(string[] args)
         {
-            Console.CursorVisible = false;
+            CursorVisible = false;
 
             bool applicationRunning = true;
-
             do
             {
-                Console.WriteLine("1. Add task");
+                WriteLine("1. Add task");
 
-                Console.WriteLine("2. Exit");
+                WriteLine("2. Exit");
 
-                ConsoleKeyInfo input = Console.ReadKey(true);
+                var input = ReadKey(true);
 
-                Console.Clear();
+                Clear();
 
                 switch (input.Key)
                 {
                     case ConsoleKey.D1:
 
+                        AddTaskMenuOption();
+
+                        Clear();
+                        
                         break;
 
                     case ConsoleKey.D2:
@@ -33,9 +40,56 @@ namespace TODO
                         break;
 
                 }
-
             } while (applicationRunning);
 
+        }
+
+        private static void AddTaskMenuOption()
+        {
+            var isAddingTask = true;
+
+            CursorVisible = true;
+
+            do
+            {
+                WriteLine("Task: ".PadLeft(10));
+                WriteLine("\nDue Date: ");
+
+                SetCursorPosition(10, 0);
+                var task = ReadLine();
+
+                SetCursorPosition(10, 2);
+                var dueDate = Convert.ToDateTime(ReadLine());
+
+                CursorVisible = false;
+
+                SetCursorPosition(0, 6);
+                WriteLine("Is this correct? (Y)es (N)o");
+
+                ConsoleKeyInfo choice;
+
+                bool invalidInput;
+                do
+                {
+                    choice = ReadKey(true);
+
+                    invalidInput = !(choice.Key == ConsoleKey.Y
+                                     || choice.Key == ConsoleKey.N);
+                } while (invalidInput);
+
+                if (choice.Key == ConsoleKey.Y)
+                {
+                    var newTask = new Tasks(task, dueDate);
+
+                    taskList.Add(newTask);
+
+                    isAddingTask = false;
+                }
+
+                Clear();
+            } while (isAddingTask);
+
+            CursorVisible = false;
         }
     }
 }
---
+++ Tasks.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TODO
+{
+    class Tasks
+    {
+        public Tasks(string task, DateTime dueDate)
+        {
+            Task = task;
+            DueDate = dueDate;
+        }
+        public string Task { get; }
+        public DateTime DueDate { get; }
+    }
+}
