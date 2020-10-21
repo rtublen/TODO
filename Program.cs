b46f90e7bdb34b59ded49632560a3572f3eb9c93
Calle Persson <Calle_1997@hotmail.se>
2020-10-21 11:29:17 +0200

    Add ability to add task

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using static System.Console;
 
 namespace TODO
 {
     class Program
     {
+        static List<Tasks> taskList = new List<Tasks>();
         static void Main(string[] args)
         {
+
+
             Console.CursorVisible = false;
 
             bool applicationRunning = true;
@@ -24,6 +30,41 @@ namespace TODO
                 {
                     case ConsoleKey.D1:
 
+                        WriteLine("Task: ");
+                        WriteLine();
+                        WriteLine("Due Date: ");
+
+                        SetCursorPosition(11, 0);
+                        string task = ReadLine();
+
+                        SetCursorPosition(11, 2);
+                        string dueDate = ReadLine();
+
+                        SetCursorPosition(0, 5);
+                        WriteLine("Is this correct? (Y)es (N)o");
+
+                        ConsoleKeyInfo yesNo;
+                        do
+                        {
+                            yesNo = ReadKey(true);
+                        } while (yesNo.Key != ConsoleKey.Y && yesNo.Key != ConsoleKey.N);
+
+                        if (yesNo.Key == ConsoleKey.Y)
+                        {
+                            Tasks tasks = new Tasks(task, DateTime.Parse(dueDate));
+
+                            taskList.Add(tasks);
+                            Clear();
+                            WriteLine("Task added.");
+                            Thread.Sleep(2000);
+                        }
+                        else
+                        {
+                            Clear();
+                            WriteLine("Task was never added.");
+                            Thread.Sleep(2000);
+                        }
+
                         break;
 
                     case ConsoleKey.D2:
@@ -33,6 +74,7 @@ namespace TODO
                         break;
 
                 }
+                Clear();
 
             } while (applicationRunning);
 
---
+++ Tasks.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TODO
+{
+    class Tasks
+    {
+        public Tasks(string task, DateTime dueDate)
+        {
+            Task = task;
+            DueDate = dueDate;
+        }
+
+        public string Task { get; }
+        public DateTime DueDate { get; }
+    }
+}
