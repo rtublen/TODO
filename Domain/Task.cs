c56edd0108f3fda35de6ae6c249e5accdddae179
Rikard Mansson <ongaskonga@hotmail.com>
2020-10-21 11:49:50 +0200

    Add taskinput

---
+++ Domain/Task.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TODO.Domain
+{
+    class Task
+    {
+
+        public Task(string addTask, DateTime dueDate)
+        {
+            AddTask = addTask;
+            DueDate = dueDate;
+        }
+
+
+
+        public string AddTask { get; }
+
+        public DateTime DueDate { get; }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,31 @@
 ﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using static System.Console;
+using TODO.Domain;
+using System.Threading;
 
 namespace TODO
 {
     class Program
     {
+
+        static List<Task> taskList = new List<Task>();
+        static int taskId = 0;
+
+
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
 
             bool applicationRunning = true;
+
+            string addTask;
+            DateTime dueDate;
+
+            bool saveTask = false;
+
+
 
             do
             {
@@ -24,6 +41,53 @@ namespace TODO
                 {
                     case ConsoleKey.D1:
 
+                        Console.Write("Task: ");
+                        addTask = Console.ReadLine();
+
+                        Console.Write("Due Date: ");
+                        string dueDateString = Console.ReadLine();
+
+                        dueDate = DateTime.ParseExact(dueDateString, "yyMMdd", CultureInfo.InvariantCulture);
+
+
+                        ConsoleKeyInfo addTaskInput;
+                        bool addTaskConfirmation = false;
+
+                        WriteLine("\nIs this correct? (Y)es (N)o");
+                        do
+                        {
+                            addTaskInput = ReadKey(true);
+                            addTaskConfirmation = (addTaskInput.Key == ConsoleKey.Y
+                                || addTaskInput.Key == ConsoleKey.N);
+
+                        } while (addTaskConfirmation == false);
+
+
+                        switch (addTaskInput.Key)
+                        {
+                            case ConsoleKey.Y:
+                                saveTask = true;
+                                break;
+
+                            case ConsoleKey.N:
+                                break;
+                        }
+
+                        if (saveTask == true)
+                        {
+
+
+                            Task task = new Task(addTask, dueDate);
+                            taskList.Add(task);
+
+                            taskId++;
+                            WriteLine("\nAdding Task");
+                            Thread.Sleep(1000);
+                            Clear();
+
+                        }
+
+
                         break;
 
                     case ConsoleKey.D2:
@@ -31,7 +95,6 @@ namespace TODO
                         applicationRunning = false;
 
                         break;
-
                 }
 
             } while (applicationRunning);
