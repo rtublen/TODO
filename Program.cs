310956babc4882994e9aadc0a81ba0ca5aef711e
Maryna <textconstruction@protonmail.com>
2020-10-21 10:24:15 +0100

    Add ability to add task

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
 
 namespace TODO
 {
@@ -6,8 +9,9 @@ namespace TODO
     {
         static void Main(string[] args)
         {
+            List<Task> todoList = new List<Task>();
+            
             Console.CursorVisible = false;
-
             bool applicationRunning = true;
 
             do
@@ -23,7 +27,45 @@ namespace TODO
                 switch (input.Key)
                 {
                     case ConsoleKey.D1:
+                        Console.WriteLine("      Task: ");
+                        Console.WriteLine("  Due Date: ");
+                        Console.CursorVisible = true;
 
+                        Console.SetCursorPosition(12, 0);
+                        string taskName = Console.ReadLine();
+
+                        Console.SetCursorPosition(12, 1);
+                        bool dueDateOk = DateTime.TryParseExact(Console.ReadLine(), "yyyy-MM-dd",
+                            CultureInfo.GetCultureInfo("sv-SE"), 0, out DateTime dueDateFormatted);
+
+                        // Set default due date value to today
+                        if (!dueDateOk) dueDateFormatted = DateTime.Today;
+
+
+                        // Request further confirmation of the input data
+                        Console.WriteLine("\n  Is this correct? (Y)es (N)o");
+                        Console.CursorVisible = false;
+                        ConsoleKeyInfo confirmation;
+                        bool confirmationIsRecognized = false;
+
+                        do
+                        {
+                            confirmation = Console.ReadKey(true);
+                            confirmationIsRecognized = confirmation.Key == ConsoleKey.Y || confirmation.Key == ConsoleKey.N;
+
+                        } while (!confirmationIsRecognized);
+
+
+                        // If confirmation is given...
+                        if (confirmation.Key == ConsoleKey.Y)
+                        {
+                            Task newTask = new Task(taskName, dueDateFormatted);
+                            todoList.Add(newTask);
+                            Console.WriteLine("\n  Task has been added");
+                            Thread.Sleep(2000);
+                        }
+
+                        Console.Clear();
                         break;
 
                     case ConsoleKey.D2:
@@ -36,6 +78,30 @@ namespace TODO
 
             } while (applicationRunning);
 
+        }
+    }
+
+    internal class Task
+    {
+        private string _taskName;
+        private DateTime _dueDate;
+
+        public Task(string taskName, DateTime dueDate)
+        {
+            TaskName = taskName;
+            DueDate = dueDate;
+        }
+
+        public string TaskName
+        {
+            get;
+            set;
+        }
+
+        public DateTime DueDate
+        {
+            get;
+            set;
         }
     }
 }
