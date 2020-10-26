7a299e74e32bd3afa53e0a35681017e70b0ae08b
Theodor Nordkvist <teddin2@outlook.com>
2020-10-26 21:06:39 +0100

    Store tasks in DB nonempty Due Date

--- MyTask.cs
+++ MyTask.cs
@@ -6,11 +6,25 @@ namespace TODO
 {
     public class MyTask
     {
+        public MyTask (string name)
+        {
+            Name = name;
+        }
+
         public MyTask(string name, DateTime dueDate)
         {
             Name = name;
             DueDate = dueDate;
         }
+
+        public MyTask(int id, string name, DateTime dueDate)
+        {
+            Id = id;
+            Name = name;
+            DueDate = dueDate;
+        }
+
+        public int Id { get; }
 
         private string name;
 
@@ -19,25 +33,25 @@ namespace TODO
             get { return name; }
             private set
             {
-                if (!string.IsNullOrWhiteSpace(value))
+                if (string.IsNullOrWhiteSpace(value))
                 {
-                    name = value;
+                     throw new ArgumentException("A task is required to have a name.", "Name");
                 }
-                else
-                {
-                    throw new ArgumentException("A task is required to have a name.", "Name");
-                }
+                name = value;
+
             }
         }
 
-        private DateTime dueDate;
+        private DateTime? dueDate;
 
-        public DateTime DueDate
+        public DateTime? DueDate
         {
             get { return dueDate; }
             private set
             {
-                if (value.Date < DateTime.Now.Date)
+
+
+                if (value != null && value.Value.Date < DateTime.Now.Date)
                 {
                     throw new ArgumentException("A new task due date must be on a future or present date.", "DueDate");
                 }
@@ -50,3 +64,5 @@ namespace TODO
 
     }
 }
+
+
--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Threading;
 
@@ -7,42 +9,68 @@ namespace TODO
 {
     class Program
     {
-        public static List<MyTask> MyTaskList = new List<MyTask>();
+        static string connectionString = "Server=localhost;Database=TODO;Integrated Security=True";
+        // TODO: Lägg till fler exampel på connection strings
 
         static void Main(string[] args)
         {
             Console.CursorVisible = false;
-
             bool applicationRunning = true;
 
             do
             {
                 Console.WriteLine("1. Add task");
-
-                Console.WriteLine("2. Exit");
-
+                Console.WriteLine("2. List tasks");
+                Console.WriteLine("3. Exit");
                 ConsoleKeyInfo input = Console.ReadKey(true);
-
                 Console.Clear();
 
                 switch (input.Key)
                 {
                     case ConsoleKey.D1:
-
                         AddTask();
-
                         break;
-
                     case ConsoleKey.D2:
-
-                        applicationRunning = false;
-
+                        ListTasks();
                         break;
-
+                    case ConsoleKey.D3:
+                        applicationRunning = false;
+                        break;
                 }
-
             } while (applicationRunning);
+        }
 
+        private static List<MyTask> FetchMyTasks()
+        {
+            string sql = "SELECT Id, Name, DueDate FROM MyTask";
+            List<MyTask> myTaskList = new List<MyTask>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(sql, connection))
+            {
+                connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var id = (int)reader["Id"];
+                    var name = (string)reader["Name"];
+                    var dueDate = (DateTime)reader["DueDate"];
+                    myTaskList.Add(new MyTask(id, name, dueDate));
+                }
+                connection.Close();
+            }
+            return myTaskList;
+        }
+		
+        private static void ListTasks()
+        {
+			var myTaskList = FetchMyTasks();
+            Console.WriteLine("Name              Due Date");
+            foreach (var myTask in myTaskList)
+            {
+                Console.WriteLine($"{myTask.Name}           {myTask.DueDate}");
+            }
+            Console.ReadKey(true);
+            Console.Clear();
         }
 
         private static void AddTask()
@@ -51,60 +79,65 @@ namespace TODO
             while (true)
             {
                 Console.Clear();
-
                 Console.CursorVisible = true;
                 Console.WriteLine("Task:  ");
                 Console.WriteLine("Due date (yyyy-MM-dd):  ");
-
-
                 Console.SetCursorPosition(36, 0);
-
-                var task = Console.ReadLine();
-
+                var taskName = Console.ReadLine();
                 Console.SetCursorPosition(36, 1);
-
                 var dueDateString = Console.ReadLine();
-
                 Console.CursorVisible = false;
-
                 Console.WriteLine("Is this correct? (Y)es (N)o ");
-
                 var isCorrectInput = Console.ReadKey(true).Key;
-
                 while (isCorrectInput != ConsoleKey.Y && isCorrectInput != ConsoleKey.N)
                 {
                     isCorrectInput = Console.ReadKey(true).Key;
                 }
-
                 if (isCorrectInput == ConsoleKey.Y)
                 {
-                    if (!string.IsNullOrWhiteSpace(dueDateString) && DateTime.TryParseExact(dueDateString, "yyyy-MM-dd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime dueDate))
+                    MyTask myTask;
+                    if (string.IsNullOrWhiteSpace(dueDateString))
                     {
-                        MyTask newTask = new MyTask(task, dueDate);
-
-
-                        MyTaskList.Add(newTask);
-
-                        Console.WriteLine("Task registered.");
-                        Thread.Sleep(2000);
+                        myTask = new MyTask(name: taskName);
+                    }
+                    else if (DateTime.TryParseExact(dueDateString, "yyyy-MM-dd", CultureInfo.CurrentCulture, DateTimeStyles.None, out DateTime dueDate))
+                    {
+                        myTask = new MyTask(name: taskName, dueDate: dueDate);
                     }
                     else
                     {
                         throw new ArgumentException("DueDate was of invalid format or empty, please use the format yyyy-MM-dd", "DueDate");
                     }
 
+                    InsertMyTask(myTask);
 
-                    
+                    Console.WriteLine("Task registered.");
+                    Thread.Sleep(2000);
                 }
                 else
                 {
                     continue;
                 }
-
                 break;
             }
             Console.Clear();
+        }
 
+        private static void InsertMyTask(MyTask myTask)
+        {
+            var sql = $@"
+                INSERT INTO MyTask (Name, DueDate)
+                VALUES (@Name, @DueDate)";
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlCommand command = new SqlCommand(sql, connection);
+
+            command.Parameters.AddWithValue("@Name", myTask.Name);
+            command.Parameters.AddWithValue("@DueDate", myTask.DueDate);
+
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
         }
     }
 }
